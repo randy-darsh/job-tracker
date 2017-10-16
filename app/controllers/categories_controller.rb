b49946ef21eadffe49cb5a9cdab7707af74c6fd8
randy-darsh <rspringer47@gmail.com>
2017-10-16 17:10:42 -0600

    add categories controller functionality

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,45 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.title} added!"
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    if @category.save
+      flash[:success] = "#{@category.title} updated!"
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title)
+  end
+end

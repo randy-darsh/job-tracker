058abaf7ada03cee2351a6ac47c29ba40c1b484e
randy-darsh <rspringer47@gmail.com>
2017-10-19 08:29:00 -0600

    add class methods

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,12 @@ class Job < ApplicationRecord
   belongs_to :company
   belongs_to :category
   has_many :comments
+
+  def self.count_by_interest
+    order(:level_of_interest).group(:level_of_interest).count
+  end
+
+  def self.count_by_city
+    order(:city).group(:city).count
+  end
 end

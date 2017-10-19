617a8543f2b57a9e66f9975dd513c73c804d6d22
randy-darsh <rspringer47@gmail.com>
2017-10-19 10:08:43 -0600

    changing seeds a bit

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,24 @@ require 'faker'
 Company.destroy_all
 Job.destroy_all
 Category.destroy_all
-Contacts.destroy_all
-Comments.destroy_all
 
 COMPANIES = ["ESPN"]
 10.times {COMPANIES << Faker::Company.name}
 JOBS = []
-20.times {JOBS << Faker::Job.field}
+100.times {JOBS << Faker::Job.field}
 CITIES = []
-10.times {CITIES << Faker::Witcher.location}
+100.times {CITIES << Faker::Witcher.location}
 CATEGORIES = ["Lame", "Not Lame", "Hella Cool", "Not Hella Cool", "Boring", "Not Boring", "Great", "Not Great"]
 COMMENTS = []
 200.times {COMMENTS << Faker::TheFreshPrinceOfBelAir.quote}
 CONTACT_NAME = []
-30.times {CONTACT_NAME << Faker::Witcher.character}
+100.times {CONTACT_NAME << Faker::Witcher.character}
 CONTACT_POSITION = []
-30.times {CONTACT_POSITION << Faker::Witcher.monster}
+100.times {CONTACT_POSITION << Faker::Witcher.monster}
 CONTACT_EMAIL = []
-30.times {CONTACT_EMAIL << Faker::Internet.email}
+100.times {CONTACT_EMAIL << Faker::Internet.email}
 DESCRIPTION = []
-30.times {DESCRIPTION << Faker::Hacker.say_something_smart}
+100.times {DESCRIPTION << Faker::Hacker.say_something_smart}
 
 CATEGORIES.each do |title|
   @category = Category.create!(title: title)

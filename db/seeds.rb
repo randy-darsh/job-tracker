515ccd3fa29e70f228e7a2a1161d8372ff7f73b2
randy-darsh <rspringer47@gmail.com>
2017-10-19 08:30:04 -0600

    replacing seeds with my own stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,22 @@ require 'faker'
 Company.destroy_all
 Job.destroy_all
 Category.destroy_all
+Contacts.destroy_all
+Comments.destroy_all
 
-COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
-JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
-CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
+COMPANIES = ["ESPN"]
+10.times {COMPANIES << Faker::Company.name}
+JOBS = []
+20.times {JOBS << Faker::Job.field}
+CITIES = []
+10.times {CITIES << Faker::Witcher.location}
 CATEGORIES = ["Lame", "Not Lame", "Hella Cool", "Not Hella Cool", "Boring", "Not Boring", "Great", "Not Great"]
 COMMENTS = []
 200.times {COMMENTS << Faker::TheFreshPrinceOfBelAir.quote}
 CONTACT_NAME = []
 30.times {CONTACT_NAME << Faker::Witcher.character}
 CONTACT_POSITION = []
-30.times {CONTACT_POSITION << Faker::Job.title}
+30.times {CONTACT_POSITION << Faker::Witcher.monster}
 CONTACT_EMAIL = []
 30.times {CONTACT_EMAIL << Faker::Internet.email}
 DESCRIPTION = []
